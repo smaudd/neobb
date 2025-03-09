f890819f99cc632de7059c4638f2a2b548a03ab9
Santiago Marcano <smarcanoweb@gmail.com>
2025-03-09 19:35:22 +0100

    chore: clean logs

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,8 +2,6 @@ import { Application } from "@hotwired/stimulus";
 
 const application = Application.start();
 
-console.log("Stimulus loaded");
-
 // Configure Stimulus development experience
 application.debug = false;
 window.Stimulus = application;
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,4 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-console.log("Enter!")
+import { application } from "controllers/application";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
